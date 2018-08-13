b483848ea09a0e70ffb6ad1e0c12e3cb44b639af
Juan P. Dantur <jpdantur@gmail.com>
2018-08-12 21:53:09 -0300

    Read Raw Image

--- src/main/java/ar/nadezhda/refractor/controller/Navigation.java
+++ src/main/java/ar/nadezhda/refractor/controller/Navigation.java
@@ -7,6 +7,7 @@
     import javafx.scene.Scene;
     import javafx.scene.image.Image;
     import javafx.scene.image.ImageView;
+    import javafx.scene.image.PixelFormat;
     import javafx.scene.image.WritableImage;
     import javafx.scene.layout.StackPane;
     import javafx.scene.paint.Color;
@@ -14,10 +15,13 @@
 
     import javax.imageio.ImageIO;
     import java.io.*;
+    import java.util.Arrays;
 
     public class Navigation {
 
-		@FXML
+        private static final int BARCO_RES =290*207;
+
+        @FXML
 		protected void clickButton(final ActionEvent event) {
 			System.out.println("click");
 		}
@@ -43,6 +47,38 @@
             Stage stage = new Stage();
             stage.setScene(scene);
             stage.show();
+
+        }
+
+        @FXML
+        protected void loadBarco(final ActionEvent event) {
+            try(FileInputStream fs = new FileInputStream("src/main/resources/BARCO.RAW")) {
+                byte[] imageBytes = new byte[BARCO_RES];
+                fs.read(imageBytes);
+                WritableImage image = new WritableImage(290,207);
+                int k=0;
+                //System.out.println(imageBytes.length);
+                //System.out.println(Arrays.toString(imageBytes));
+                for (int h=0;h<207;h++){
+                    for (int w=0;w<290;w++){
+
+                        image.getPixelWriter().setColor(w,h,Color.grayRgb(Byte.toUnsignedInt(imageBytes[k])));
+                        k++;
+                    }
+                }
+                //image.getPixelWriter().setPixels(0,0,290,207,PixelFormat.getByteBgraPreInstance(),imageBytes,0,290);
+                ImageView imageView = new ImageView();
+                imageView.setImage(image);
+                StackPane root = new StackPane();
+                root.getChildren().add(imageView);
+                Scene scene = new Scene(root, 290, 207);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         }
 	}
